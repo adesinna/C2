9b8bb1fd912af48055891364744581f8e663e58d
Adesina Aladesae <101404177+adesinna@users.noreply.github.com>
2025-02-05 11:09:01 -0500

    Update nginx.sh

--- vagrant-dev/provision/shell/nginx.sh
+++ vagrant-dev/provision/shell/nginx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # encryption
-sudo ln -snf /opt/c2/encryption-dev /var/www/encryption
+sudo ln -snf /opt/c2/encryption-dev /var/www/encryption # Fix the sym link to point to the right folder
 sudo cp /vagrant/provision/confs/nginx/encryption.conf /etc/nginx/sites-available/encryption
 sudo chmod 644 /etc/nginx/sites-available/encryption
 sudo ln -snf /etc/nginx/sites-available/encryption /etc/nginx/sites-enabled/encryption
